ce1f95823f4c7061d9f1d03e5c820a7b2a16d83b
TYD <goldcyber.fenz@gmail.com>
2026-01-20 12:01:45 +0700

    edit ลิ้งกายภาพบำบัด

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -18,12 +18,11 @@
         </div>
       </div>
       <!-- Sidebar Menu -->
-       
+
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-               
+          <!-- Add icons to the links using the .nav-icon class with font-awesome or any other icon font library -->
+          
           <li class="nav-header"><h3><i class="nav-icon fas fa-tachometer-alt"> </i>  ภาพรวมระบบ</h3></li>
                 <li class="nav-item"><a href="?page=overview" class="nav-link "><i class="nav-icon fas fa-tachometer-alt"></i><p>Dashboard หน้าหลัก</p></a></li>
 
@@ -34,8 +33,8 @@
                 <li class="nav-item"><a href="?page=eclaim_lgo" class="nav-link "><i class="fas fa-file-export nav-icon text-info"></i><p>เคลมสิทธิ LGO</p></a></li>
                 <li class="nav-item"><a href="?page=fsfund" class="nav-link "><i class="fas fa-coins nav-icon text-warning"></i><p>ชดเชย FS</p></a></li>
                 <li class="nav-item"><a href="?page=moph_claim_report" class="nav-link "><i class="fas fa-hospital-user nav-icon text-primary"></i><p>ชดเชย MOPH Claim</p></a></li>
-                <li class="nav-item"><a href="?page=thai_fm_annual" class="nav-link "><i class="fas fa-leaf nav-icon text-success"></i><p>ชดเชยกายภาพบำบัด</p></a></li>
-                                <li class="nav-item"><a href="?page=thai_fm_annual" class="nav-link "><i class="fas fa-leaf nav-icon text-success"></i><p>ชดเชยแพทย์แผนไทย</p></a></li>
+                <li class="nav-item"><a href="?page=pt_report" class="nav-link "><i class="fas fa-leaf nav-icon text-success"></i><p>ชดเชยกายภาพบำบัด</p></a></li>
+                <li class="nav-item"><a href="?page=thai_fm_annual" class="nav-link "><i class="fas fa-leaf nav-icon text-success"></i><p>ชดเชยแพทย์แผนไทย</p></a></li>
                 <li class="nav-item"><a href="?page=sss_report_02991" class="nav-link "><i class="fas fa-user-shield nav-icon text-indigo"></i><p>ชดเชย ปกส.ในจังหวัด</p></a></li>
                 <li class="nav-item"><a href="?page=car_act_report" class="nav-link "><i class="fas fa-car-crash nav-icon text-danger"></i><p>ชดเชย พรบ.รถ</p></a></li>
           <li class="nav-item ">
@@ -50,5 +49,14 @@
 
     
   </aside>
-
-  
+  <script>
+  // เมื่อโหลดหน้าเว็บเสร็จ ให้สั่งพับเมนูทันที
+  $(document).ready(function() {
+    $('[data-widget="pushmenu"]').PushMenu('collapse');
+    
+    // ป้องกันกรณี AdminLTE จำสถานะเดิมจาก LocalStorage (ถ้ามี)
+    if (typeof(Storage) !== "undefined") {
+      localStorage.setItem('remember_adminlte_sidebar', 'collapsed');
+    }
+  });
+</script>
